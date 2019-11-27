daa7c27309d6a81eab13379e9d5babf947fe60fd
Coqueiro <kartgarcia@gmail.com>
2019-11-27 18:44:39 -0300

    Changing to use github action environment variables pattern

--- jenkins_job.py
+++ jenkins_job.py
@@ -1,13 +1,13 @@
 from jenkins_helper.jenkins_functions import *
 import json
 
-jenkins_url = os.environ["jenkins_url"]
-jenkins_user = os.environ["jenkins_user"]
-jenkins_token = os.environ["jenkins_token"]
-job_name = os.environ["job_name"]
-jenkins_params = os.environ.get("jenkins_params")
-console_log_regex = os.environ.get("console_log_regex")
-console_log_regex_group = os.environ.get("console_log_regex_group")
+jenkins_url = os.environ["INPUT_JENKINS_URL"]
+jenkins_user = os.environ["INPUT_JENKINS_USER"]
+jenkins_token = os.environ["INPUT_JENKINS_TOKEN"]
+job_name = os.environ["INPUT_JOB_NAME"]
+jenkins_params = os.environ.get("INPUT_JENKINS_PARAMS")
+console_log_regex = os.environ.get("INPUT_CONSOLE_LOG_REGEX")
+console_log_regex_group = os.environ.get("INPUT_CONSOLE_LOG_REGEX_GROUP")
 
 
 def main():
